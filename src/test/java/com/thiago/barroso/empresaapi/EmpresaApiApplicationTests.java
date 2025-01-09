0a36d3d14e86dc4aadef732b0f2f529a6fa16cda
Thiago Barroso <memoriathiago@gmail.com>
2025-01-09 17:14:00 -0300

    Início do projeto

---
+++ src/test/java/com/thiago/barroso/empresaapi/EmpresaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.barroso.empresaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
