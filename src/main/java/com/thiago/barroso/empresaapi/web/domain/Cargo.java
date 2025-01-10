5812fa9bf5d439c6e620c9ddff2bdeb0e046ed78
Thiago Barroso <memoriathiago@gmail.com>
2025-01-09 23:03:11 -0300

    Criação das classes do domain

---
+++ src/main/java/com/thiago/barroso/empresaapi/web/domain/Cargo.java
@@ -0,0 +1,51 @@
+package com.thiago.barroso.empresaapi.web.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "CARGOS")
+public class Cargo extends AbstractEntity<Long> {
+	
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+	
+	@ManyToOne	
+	@JoinColumn(name = "id_departamento_fk")
+	private Departamento departamento;
+	
+	@OneToMany(mappedBy = "cargo")
+	private List<Funcionario> funcionarios;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	
+}
